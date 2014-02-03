bb1ec4b04012ff215f28aa42e96ec427d570c52f
Jacob Lowe <jacoblowe2.0@gmail.com>
2014-02-02 23:16:04 -0800

    future planning next phase

---
+++ libs/deps.js
@@ -0,0 +1,44 @@
+var spawn = require('child_process').spawn;
+var execute = require('child_process').exec;
+
+function Deps ( url, callback ) {
+	this.url = url;
+	console.log( this.url );
+	this.removeDeps(function(){
+		this.install( callback )
+	}.bind( this ))
+}
+
+Deps.prototype.install = function ( callback ) {
+	var install = spawn( 'npm', ['install'], { cwd : this.url } );
+	install.stderr.on( 'data', function ( data ) {
+		if ( !data || !data.length ) return;
+		console.log( data.toString('utf8') );
+	});
+	install.stdout.on( 'data', function ( data ) {
+		if ( !data || !data.length ) return;
+		console.log( data.toString('utf8') );
+	});
+	install.on('close', function(){
+		console.log('close');
+		if ( callback ) callback ( );
+	});
+};
+
+Deps.prototype.removeDeps = function ( callback ) {
+	var rm = spawn( 'rm', ['-rf', this.url + '/node_modules' ] );
+	rm.stderr.on( 'data', function ( data ) {
+		if ( !data || !data.length ) return;
+		console.log( data.toString('utf8') );
+	});
+	rm.stdout.on( 'data', function ( data ) {
+		if ( !data || !data.length ) return;
+		console.log( data.toString('utf8') );
+	});
+	rm.on('close', function(){
+		console.log('close');
+		callback( );
+	});
+};
+
+module.exports = Deps;
---
+++ libs/startup.js
@@ -0,0 +1,7 @@
+/*
+This file is to do the last few parts of deploying
+first we need to run test specified in `package.json.scripts.test`
+then run a start script, i think for now just generate a open port 
+and run. Might be a good idea to use forever or something to manage 
+the lower level work like pid mamangment.
+*/
